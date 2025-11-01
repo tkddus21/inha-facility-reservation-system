fbbd6851fb5b2810ff28964a049ea55bed65f80c
tkddus21 <sangyeoni985@gmail.com>
2025-11-01 16:45:16 +0900

    tkddus21's local pc에서 돌릴 때는master로 sql 계정을 만들어서 했습니다.

--- backend/database.py
+++ backend/database.py
@@ -6,16 +6,16 @@ import os
 # --- (중요) MySQL 연결 설정 ---
 # DDL.sql 파일은 MySQL 기준이므로, MySQL에 연결합니다.
 
-MYSQL_USER = "root" # 1. 본인의 MySQL 사용자 이름 (보통 root)
-MYSQL_PASSWORD = "8475" # 2. ⚠⚠⚠ 이 부분을 본인의 MySQL 비밀번호로 변경하세요! ⚠⚠⚠
+MYSQL_USER = "master" # 1. 본인의 MySQL 사용자 이름 (보통 root)
+MYSQL_PASSWORD = "1234" # 2. ⚠⚠⚠ 이 부분을 본인의 MySQL 비밀번호로 변경하세요! ⚠⚠⚠
 MYSQL_HOST = "127.0.0.1" # 로컬 호스트
 MYSQL_DB = "inha_reserv" # Workbench에서 만든 DB 이름
 
 # mysql+mysqlclient://{유저이름}:{비밀번호}@{서버주소}:{포트}/{DB이름}
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:3306/{MYSQL_DB}"
+# SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:3306/{MYSQL_DB}"
 
 # (참고) 만약 'mysqlclient' 설치가 안돼서 'PyMySQL'을 설치했다면 아래 주석을 대신 사용하세요.
-# SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:3306/{MYSQL_DB}"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:3306/{MYSQL_DB}"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
